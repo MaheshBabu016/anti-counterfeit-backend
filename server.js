ac42584a766742fc1a5668892c9f44a7dc083311
Maheshbabu016 <jshivanarayanareddy@gmail.com>
2025-11-23 17:28:04 +0530

    fix route paths

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const verifyRoute = require("./routes/verify");
 const registerRoute = require("./routes/register");
 
 const app = express();
+
+// middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/", verifyRoute);
-app.use("/", registerRoute);
+// routes
+app.use("/verify", verifyRoute);
+app.use("/register", registerRoute);
 
+// server
 app.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
 });
